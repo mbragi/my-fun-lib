08d5bce93397aa0736321c52b32a5f1476a5d4b4
mbragi <spontaneousmanji@gmail.com>
2025-01-10 15:12:50 +0100

    chore: updating

---
+++ index.mjs
@@ -0,0 +1,2 @@
+export * as Math from "./math-lib.mjs";
+export * as Algo from "./algo-lib.mjs";
---
+++ test.mjs
@@ -0,0 +1,9 @@
+import { Math, Algo } from "./index.mjs";
+console.log(Math.add(1, 2))
+console.log(Math.subtraction(1, 2))
+console.log(Math.division(9, 2))
+console.log(Math.findSqrt(9))
+console.log(Math.multiplication(9, 7))
+console.log(Math.findPerimeterOfSquare(6))
+console.log(Algo.minMax([20,5,3,6,100, 38, 24]))
+console.log(Algo.sort([20,5,3,6,100, 38, 24], 'DESC'))
